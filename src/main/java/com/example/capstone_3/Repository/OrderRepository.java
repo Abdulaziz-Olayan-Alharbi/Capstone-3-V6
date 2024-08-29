aa8174f3b7f175a858ea9e0d112c9b38e6115be7
Abdulaziz Alharbi <a.olayanalharbi@gmail.com>
2024-08-29 09:59:42 +0300

    Add files via upload

---
+++ src/main/java/com/example/capstone_3/Repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package com.example.capstone_3.Repository;
+
+import com.example.capstone_3.Model.*;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    Order findOrderById(Integer id);
+    List<Order> findOrderByCustom(Customer customer);
+    List<Order> findOrderByFabric(Fabric fabric);
+    List<Order> findOrderByDesigner(Designer designer);
+    List<Order> findOrderByMerchant(Merchant merchant);
+    List<Order> findOrderByTailor(Tailor tailor);
+    @Query("SELECT o.fabric FROM Order o GROUP BY o.fabric.id ORDER BY COUNT(o.id) DESC")
+    List<Fabric> findBestSellingFabric(Pageable pageable);
+    List<Order> findOrderByOrderStatus(String orderStatus);
+    ///
+    List<Order> findOrderByFabricId(Integer fabricId);
+    List<Order> findOrderByCustomId(Integer customerId);
+
+}
